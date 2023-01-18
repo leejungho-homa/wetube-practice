b8844cb70c3ff35b3092da3687605df4529e9893
leejungho-homa <leejho1107@naver.com>
2023-01-18 23:18:42 +0900

    wetube-practice
    2023.01.18

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./src/routers/globalRouter";
+import videoRouter from "./src/routers/videoRouter";
+import userRouter from "./src/routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 
-const globalRouter = express.Router();
-const userRouter = express.Router();
-const videoRouter = express.Router();
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
-
-const home = (req, res) => {
-  return res.send("hello");
-};
-
-const login = (req, res) => {
-  return res.send("login");
-};
-
-app.use(logger);
-app.get("/", home);
-app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listenting on port http://localhost:${PORT}`);
---
+++ src/routers/userRouter.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { edit, remove } from "../controllers/userController";
+
+const userRouter = express.Router();
+
+export default userRouter;
