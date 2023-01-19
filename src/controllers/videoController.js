10f07f9f47e6466215094280b30179db85352183
leejungho-homa <leejho1107@naver.com>
2023-01-19 22:10:28 +0900

    2023.01.19
    wetube-practice

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,17 @@
-export const trending = (req, res) => res.render("home");
+const fakeUser = {
+  username: "Jungho",
+  loggedIn: true,
+};
+
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
 export const see = (req, res) => {
   console.log(req.params);
-  res.render("watch");
+  res.render("watch", { pageTitle: "Watch" });
 };
-export const edit = (req, res) => res.send("Edit");
+export const edit = (req, res) => res.render("Edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { join } from "../controllers/userController";
+import { login } from "../controllers/userController";
+import { logout } from "../controllers/userController";
 import { trending } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/logout", logout);
 
 export default globalRouter;
