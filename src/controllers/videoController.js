f5b066e7744c9e7e6796308c1026e73f83bf7620
leejungho-homa <leejho1107@naver.com>
2023-01-20 20:54:29 +0900

    2023.01.20
    wetube-practice

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,46 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 10,
+    comments: 5,
+    createAt: "3 minutees ago",
+    views: 60,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 8,
+    comments: 10,
+    createAt: "1 minutees ago",
+    views: 55,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutees ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 const fakeUser = {
   username: "Jungho",
   loggedIn: true,
 };
 
 export const trending = (req, res) => {
-  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   res.render("home", { pageTitle: "Home", fakeUser, videos });
 };
-export const see = (req, res) => {
-  console.log(req.params);
-  res.render("watch", { pageTitle: "Watch" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", {
+    pageTitle: `Watching ${video.title}`,
+    fakeUser,
+    video,
+  });
 };
 export const edit = (req, res) => res.render("Edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
